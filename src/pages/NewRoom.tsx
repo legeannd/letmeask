febc960be1831809937c4a351833f2c5680ea345
Gean Lucas <gean.lfpsa@gmail.com>
2021-06-21 15:59:45 -0300

    Created navigation between pages using React Router DOM

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { Button } from '../components/Button';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -26,7 +28,7 @@ export function NewRoom() {
               Criar sala
             </Button>
           </form>
-          <p>Quer entrar em uam sala existente? <a href="#">clique aqui</a></p>
+          <p>Quer entrar em uam sala existente? <Link to="/">clique aqui</Link></p>
         </div>
       </main>
     </div>
